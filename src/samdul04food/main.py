07ee5f3b4ba5cdb369e4af29e3314dc16e5434ae
oddsummer56 <oddsummer56@gmail.com>
2024-09-13 15:56:38 +0900

    cros

--- src/samdul04food/main.py
+++ src/samdul04food/main.py
@@ -2,9 +2,21 @@ from typing import Union
 from fastapi import FastAPI
 import pickle
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8899",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"Hello": "n04"}
